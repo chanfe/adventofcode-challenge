9c8d775fd2e8f4867f6a68102942b996b070bbac
Felix Chan <felixchan29@gmail.com>
2019-01-11 02:02:56 -0500

    test

--- codes/day_2.py
+++ codes/day_2.py
@@ -3,9 +3,25 @@ def question1(file):
         content = f.readlines()
         content = [x.strip() for x in content]
         answer = 0
+        dic = {}
+        all_two = 0
+        all_three = 0
         for i in content:
-            answer += int(i)
-        print (answer)
+            for j in i:
+                if j in dic:
+                    dic[j] = dic[j] + 1
+                else:
+                    dic[j] = 1
+
+            for x in dic:
+                if dic[x] == 2:
+                    all_two += 1
+                elif dic[x] == 3:
+                    all_three += 1
+            print (dic, all_two, all_three)
+            dic = {}
+        print (dic)
+        print (all_two * all_three)
 
 def question2(file):
     with open(file) as f:
@@ -23,6 +39,3 @@ def question2(file):
             i += 1
 
 print(question1('../files/test.txt'))
-print(question1('../files/day2_input.txt'))
-print(question2('../files/test.txt'))
-print(question2('../files/day2_input.txt'))
