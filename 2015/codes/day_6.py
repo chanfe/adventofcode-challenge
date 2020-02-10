4de859844973aa0ba6530a1eb3336b135eec4deb
Felix Chan <felixchan29@gmail.com>
2020-02-10 13:04:21 -0500

    first question done

--- 2015/codes/day_6.py
+++ 2015/codes/day_6.py
@@ -29,9 +29,85 @@ def question1(file):
             key_word = re.search('(on|off|toggle)', i)
             x1,y1 = coordinate[0].split(',')
             x2,y2 = coordinate[1].split(',')
+            x1 = int(x1)
+            x2 = int(x2) +1
+            y1 = int(y1)
+            y2 = int(y2) +1
 
-            
-            print(key_word.group())
+            if key_word.group() == 'on':
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 1
+            elif key_word.group() == 'off':
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 0
+            else:
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 1 - grid[i][j]
+        total = 0
+        print(grid)
+        for i in range(1000):
+            for j in range(1000):
+                if grid[i][j] == 1:
+                    total += 1
+        return total
+
+# --- Part Two ---
+# You just finish implementing your winning light pattern when you realize you mistranslated Santa's message from Ancient Nordic Elvish.
+
+# The light grid you bought actually has individual brightness controls; each light can have a brightness of zero or more. The lights all start at zero.
+
+# The phrase turn on actually means that you should increase the brightness of those lights by 1.
+
+# The phrase turn off actually means that you should decrease the brightness of those lights by 1, to a minimum of zero.
+
+# The phrase toggle actually means that you should increase the brightness of those lights by 2.
+
+# What is the total brightness of all lights combined after following Santa's instructions?
+
+# For example:
+
+# turn on 0,0 through 0,0 would increase the total brightness by 1.
+# toggle 0,0 through 999,999 would increase the total brightness by 2000000.
+
+def question2(file):
+    with open(file) as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
+        grid = []
+        for i in range(1000):
+            grid.append([0] * 1000)
+        for i in content:
+            coordinate = re.findall('\d{1,3},\d{1,3}', i)
+            key_word = re.search('(on|off|toggle)', i)
+            x1,y1 = coordinate[0].split(',')
+            x2,y2 = coordinate[1].split(',')
+            x1 = int(x1)
+            x2 = int(x2) +1
+            y1 = int(y1)
+            y2 = int(y2) +1
+
+            if key_word.group() == 'on':
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 1
+            elif key_word.group() == 'off':
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 0
+            else:
+                for i in range(y1,y2):
+                    for j in range(x1,x2):
+                        grid[i][j] = 1 - grid[i][j]
+        total = 0
+        print(grid)
+        for i in range(1000):
+            for j in range(1000):
+                if grid[i][j] == 1:
+                    total += 1
+        return total
 
 
 print(question1('../files/day6_input.txt'))
